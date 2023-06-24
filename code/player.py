940278360e5a4dee85295207c30895d7ec96f6e8
seto yuma <setoichi.dev@gmail.com>
2023-06-24 16:41:11 -0700

    welp player?

---
+++ code/player.py
@@ -0,0 +1,6 @@
+from BLACKFORGE2 import *
+
+class Player(Entity):
+	def __init__(self, size:int, position:tuple, speed:int, groups:list):
+		super().__init__(self, size, position, speed, groups)
+		
