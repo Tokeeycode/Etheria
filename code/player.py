ed5bf35fde8bbb7b4559bda28d3ccc9635ddba4f
Tokeeycode <ethanoneill66@gmail.com>
2023-06-26 23:01:51 -0400

    correct movement

--- code/player.py
+++ code/player.py
@@ -5,7 +5,7 @@ class Player(Entity):
 		super().__init__(size, position, speed, groups)
 		self.game = game
 		self.image = get_image('../assests/player/idle/Idle1.png').convert_alpha()
-		self.image = scale_images([self.image], (46, 46))[0]
+		self.image = scale_images([self.image], (32, 32))[0]
 		self.rect = self.image.get_rect(topleft=self.position)
 		self.collision_area = pygame.Rect(self.rect.topleft, (96, 96))
 	
@@ -15,13 +15,14 @@ class Player(Entity):
 			self.velocity.y = -self.speed * dt
 		elif keys[pygame.K_s]:
 			self.velocity.y = self.speed * dt
-		elif keys[pygame.K_a]:
+		else:
+			self.velocity.y = 0
+		if keys[pygame.K_a]:
 			self.velocity.x = -self.speed * dt
 		elif keys[pygame.K_d]:
 			self.velocity.x = self.speed * dt
 		else:
 			self.velocity.x = 0
-			self.velocity.y = 0
    
 	def update(self, dt):
 		self.move(dt)
@@ -29,3 +30,5 @@ class Player(Entity):
 		self.physics.horizontal_movement_collision(self, self.game.level.terrain)
 		self.rect.y += self.velocity.y
 		self.physics.vertical_movement_collision(self, self.game.level.terrain)
+		if self.velocity.magnitude() != 0:
+			self.velocity = self.velocity.normalize()
